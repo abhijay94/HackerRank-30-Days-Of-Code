087690dc198a2cc911182df4370fa51a81f507ed
abhijay94 <abhijay94@gmail.com>
2018-03-23 18:31:50 +0530

    Added solution for Day 8

---
+++ src/Day8.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+class Day8 {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        HashMap<String, Integer> contacts = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String name = in.next();
+            int phone = in.nextInt();
+            contacts.put(name, phone);
+        }
+        while (in.hasNext()) {
+            String s = in.next();
+            if(contacts.containsKey(s)){
+                System.out.println(s + "=" + contacts.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+}
