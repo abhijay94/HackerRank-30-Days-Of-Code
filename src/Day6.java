42f248be770b300dd4a7f88e8b0783020d3f2939
abhijay94 <abhijay94@gmail.com>
2018-03-23 17:54:23 +0530

    Added solution for Day 6

---
+++ src/Day6.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Day6 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n > 0) {
+            String input = sc.next();
+            String odd = "";
+            String even = "";
+
+            char[] input_arr = input.toCharArray();
+            for (int i = 0; i < input_arr.length; i++) {
+                if (i % 2 == 0) {
+                    odd = odd + input_arr[i];
+                } else {
+                    even = even + input_arr[i];
+                }
+            }
+            System.out.println(odd + " " + even);
+            n--;
+        }
+        sc.close();
+    }
+}
