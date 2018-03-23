67850a7023f4f28fb5fe65bd7e4b5d1bb4193226
abhijay94 <abhijay94@gmail.com>
2018-03-23 15:08:10 +0530

    Added solution for Day 5

--- src/Day5.java
+++ src/Day5.java
@@ -5,6 +5,7 @@ public class Day5 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        
         for (int i = 1; i <= 10; i++) {
             System.out.println(n + " x " + i + " = " + i * n);
         }
