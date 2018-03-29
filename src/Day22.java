6695dffb076f9865468a20f21b63c9477eda0f6d
abhijay94 <abhijay94@gmail.com>
2018-03-29 13:06:43 +0530

    Added solution for Day 22- BST Height --Updated to editorial solution

--- src/Day22.java
+++ src/Day22.java
@@ -12,17 +12,11 @@ class Node {
 
 class Day22 {
     private static int getHeight(Node root) {
-        int heightLeft = 0;
-        int heightRight = 0;
-
-        if (root.left != null) {
-            heightLeft = getHeight(root.left) + 1;
+        if (root == null) {
+            return -1;
+        } else {
+            return 1 + Math.max(getHeight(root.left), getHeight(root.right));
         }
-        if (root.right != null) {
-            heightRight = getHeight(root.right) + 1;
-        }
-
-        return heightLeft > heightRight ? heightLeft : heightRight;
     }
 
     public static Node insert(Node root, int data) {
