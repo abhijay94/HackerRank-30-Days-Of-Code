f84d02510abac403e0fdae253fc8a00efc3f3a0d
abhijay94 <abhijay94@gmail.com>
2018-04-06 10:18:33 +0530

    Added solution for Day 27 - Testing

---
+++ src/Day27.java
@@ -0,0 +1,10 @@
+public class Day27 {
+
+    public static void main(String[] args) {
+        System.out.println("2");
+        System.out.println("4 3");
+        System.out.println("-1 -3 4 2");
+        System.out.println("5 2");
+        System.out.println("0 -1 2 1 4");
+    }
+}
